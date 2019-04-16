25387f4f27e614f98798fc0739a7fcc30d3aa901
Oliver Davies <oliver@oliverdavies.uk>
2019-04-16 19:05:09 +0100

    Configure PurgeCSS
    Reduces the compiled file size from 355 KB to 4 KB!

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,13 @@
 const mix = require('laravel-mix')
+const glob = require('glob-all');
+
+require('laravel-mix-purgecss')
 
 mix.postCss('src/css/app.css', 'dist/css', [
   require('tailwindcss')()
 ])
+.purgeCss({
+  paths: () => glob.sync([
+    path.join(__dirname, './index.html')
+  ])
+})
